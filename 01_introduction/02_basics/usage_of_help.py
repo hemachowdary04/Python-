ca0a5fd74a486a3e1788493c55097a09dd39c356
hemachowdary04 <133361048+hemachowdary04@users.noreply.github.com>
2025-06-27 04:27:31 +0000

    class08

---
+++ 01_introduction/02_basics/usage_of_help.py
@@ -0,0 +1,27 @@
+dozen = 12
+dozen = 12.3
+dozen = None
+dozen = True
+
+print(f"{type(dozen) =} ")
+print(f"{id(dozen) =} ")
+print(f"{dozen =} ")
+print(f"{dir(dozen) =} ")
+
+help(dozen)
+
+mountain = "himalayas"
+print(f"{type(mountain) =} ")
+print(f"{id(mountain) =} ")
+print(f"{mountain =} ")
+print(f"{dir(mountain) =} ")
+
+help(mountain)
+help(str)
+
+#for only strings help works differently we have to pass str inside the 
+
+#python -h shows all the help
+
+# to start a local server for help in some free port
+#       python -m pydoc -b
---
+++ 02_language_components/a_relational_operations.py
@@ -0,0 +1,16 @@
+us_dollar = 86
+canadian_dollar = 50
+
+print("us_dollar = ",us_dollar)
+print("canadian_dollar = ",canadian_dollar)
+
+print(f"us_dollar = {us_dollar}")
+print(f"canadian_dollar = {canadian_dollar}")
+print()
+
+print(f"{us_dollar == canadian_dollar =}")
+print(f"{us_dollar > canadian_dollar =}")
+print(f"{us_dollar >= canadian_dollar =}")
+print(f"{us_dollar < canadian_dollar =}")
+print(f"{us_dollar <= canadian_dollar =}")
+#print(f"{us_dollar <> canadian_dollar =}")
